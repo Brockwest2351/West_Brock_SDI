cdaeb5835e59c8992e75f2fe74de290ab6d70cf8
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-09 19:06:41 -0400

    created starting files for day 2
    created files in web storm

---
+++ Day2/js/script.js
@@ -0,0 +1,19 @@
+/*
+this is going to be a multi lined comment
+Brock West
+7-9-15
+SDI Section 00
+Day 2 Coding Basics
+ */
+alert("This is making sure our js file is connected!");
+
+//This is asing lined comment
+// Usually will describe what comes right after
+
+/*
+Multi-lined comment
+Anything between the stars and slashes is ignored by the browsers
+ */
+
+//Alert- gives the user information but returns nothing to us
+alert("Welcome to SDI");
