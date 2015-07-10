7fde58fd3c97b8f37ef0d6222aa6cd50aa45d2ac
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-10 00:06:38 -0400

    Day 3 expressions final
    final of day 3 expressions

--- Day3Expressions/homeworkExpressions.js
+++ Day3Expressions/homeworkExpressions.js
@@ -30,8 +30,30 @@ console.log ("slices sparky ate " + sparky);
 
 // Total amount spent 1000
 
-var total = 1000;
 var weeks = 5;
-var average = total/weeks;
 
-console.log("Average grocery bill over 5 weeks" average);
+var week1= 200;
+var week2=210;
+var week3=220;
+var week4=230;
+var week5=240;
+
+var total= week1+week2+week3+week4+week5;
+var average = total/weeks;
+console.log("total amount spent " +total,"Average grocery bill over 5 weeks " + average);
+
+// Discounts
+
+var itemCost = 1000;
+var discount = 20;
+var discountPercent = discount / 100;
+var salesTax =.05;
+
+var discountOver = itemCost*discountPercent;
+var noTax = itemCost-discountOver;
+
+
+var Tax = noTax*salesTax;
+var afterTax= Tax+noTax;
+
+console.log ("Item cost "+ itemCost, "discount percentage "+discount,"with no sales tax "+noTax,"with sales tax "+afterTax);
