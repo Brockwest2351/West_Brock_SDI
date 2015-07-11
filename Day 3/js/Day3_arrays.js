d031ad973c72e7201632b8875ab49fdf6521db6d
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-11 17:51:28 -0400

    Basic array
    addition of array items

--- Day 3/js/Day3_arrays.js
+++ Day 3/js/Day3_arrays.js
@@ -7,3 +7,18 @@ Arrays
 
 //alert("day 3 arrays");
 
+// Create a basic array
+// Picking Oranges
+var orangeBins = [30,500,10002];
+
+//How many oranges did we pick?
+var totalOranges = orangeBins [0] + orangeBins [1] + orangeBins [2];
+console.log[totalOranges + " Oranges were picked in 3 days. "];
+
+//Average number of oranges picked
+
+var averageOranges = totalOranges/orangeBins.length;
+console.log(" The average number of oranges picked is " + averageOranges);
+
+// Length property of arrays
+console.log(orangeBins.length);
