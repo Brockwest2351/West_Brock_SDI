1cd1f534652ec7949175e7cbbfbdda01f1152ee5
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-11 11:17:21 -0400

    beginning of assignment
    start of assignment

---
+++ Day 3/js/expressionsAssignment.js
@@ -0,0 +1,8 @@
+/*
+Brock West
+Section 00
+11Jul15
+Expressions Assignment
+ */
+
+alert("testing to see if this work!");
---
+++ West_Brock_Expressions_Assignment/js/expressionsAssignment.js
@@ -0,0 +1,9 @@
+
+ /*
+Brock West
+Section 00
+Date
+Topic
+ */
+
+alert("testing to see if this work!");
