3876109382bdf670c003bff4fd4648052b9e964b
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-11 12:39:31 -0400

    added test
    added my test code

--- West_Brock_Expressions_Assignment/js/expressionsAssignment.js
+++ West_Brock_Expressions_Assignment/js/expressionsAssignment.js
@@ -12,7 +12,7 @@ Expressions
 var gameOneAttempts= prompt (" Let's calculate your shooting average over the last 3 basketball games.\nHow many shooting attempts did you have for game one? ");
 console.log (gameOneAttempts);
 
-var gameOneMade= prompt (" How many shots did you make in game one?");
+var gameOneMade= prompt (" How many shots did you make in game one? ");
 console.log (gameOneMade);
 
 var gameTwoAttempts= prompt (" How many shots did you attempt in game two? ");
@@ -27,10 +27,27 @@ console.log (gameThreeAttempts);
 var gameThreeMade= prompt (" How many shots did you make in game three? ");
 console.log (gameThreeMade);
 
+// Array of shots made
+
+var shotsAttempted= [" game one attempts "+gameOneAttempts," game two attempts "+gameTwoAttempts," game three attempts " + gameThreeAttempts];
+console.log (shotsAttempted);
+var shotsMade= [" game one shots made " +gameOneMade," game two shots made " +gameTwoMade," game three shots made " +gameThreeMade];
+console.log (shotsMade);
+
 // adding shots attempted over all three games
 
 var totalAttempted= (Number (gameOneAttempts)+ Number (gameTwoAttempts)+ Number (gameThreeAttempts));
 alert (" total shots attempted over 3 games " + totalAttempted);
 
-var totalMade =(Number (gameOneMade)+ Number (gameTwoMade)+ Number (gameThreeMade))
-alert ("total shots made over 3 games" + totalMade)
+var totalMade =(Number (gameOneMade)+ Number (gameTwoMade)+ Number (gameThreeMade));
+alert ("total shots made over 3 games " + totalMade);
+
+var shootingPercentage= totalMade/=totalAttempted;
+console.log ("Move the decimal two places and you have your shooting percentage "+ shootingPercentage);
+
+/*
+Test
+Shots attempted game 1:30 game 2:29 game 3:28
+Shots Made game 1:20 game 2:19 game 3:18
+Shooting percentage: 65.5
+ */
