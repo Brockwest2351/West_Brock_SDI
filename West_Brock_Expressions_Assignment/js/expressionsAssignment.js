41b2cfd0101f3ed2033468f1e9894d72753c81c5
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-11 11:28:59 -0400

    game one shots
    game one shots attempted and shots made

--- West_Brock_Expressions_Assignment/js/expressionsAssignment.js
+++ West_Brock_Expressions_Assignment/js/expressionsAssignment.js
@@ -1,9 +1,16 @@
-
- /*
+/*
 Brock West
 Section 00
-Date
-Topic
+11 Jul 15
+Expressions
  */
 
-alert("testing to see if this work!");
+//alert("testing to see if this work!");
+
+// calculating my basketball shooting percentage over 5 games
+
+var gameOneAttempts= prompt (" Let's calculate your shooting average over the last 3 basketball games.\nHow many shooting attempts did you have for game one? ");
+console.log (gameOneAttempts);
+
+var gameOnemade= prompt (" How many shots did you make in game one?");
+console.log (gameOnemade);
