75c3b23744630c15f255398e74c4c51b039c050f
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-14 07:51:47 -0400

    finished covering else if statements
    else if

--- day 4/js/day4conditionals.js
+++ day 4/js/day4conditionals.js
@@ -1,8 +1,8 @@
 /*
-Brock West
-Section 00
-14 JUL 15
-Conditionals
+ Brock West
+ Section 00
+ 14 JUL 15
+ Conditionals
  */
 
 //alert("testing to see if this work!");
@@ -18,29 +18,30 @@ var oldEnough = true;
 // if the child is old enough, tell him he can ride the coaster
 // else {this will run if the IF statement does not run}
 if(oldEnough){
-  //code inside of the {} will run if oldEnough is a boolean of true
- console.log("you can ride the coaster!!")
+    //code inside of the {} will run if oldEnough is a boolean of true
+    console.log("you can ride the coaster!!")
 } else {
- // this code will run IF the IF block does not run
- // if old enough evaluates to a boolean of false
- console.log("sorry you are too young to ride.")
+    // this code will run IF the IF block does not run
+    // if old enough evaluates to a boolean of false
+    console.log("sorry you are too young to ride.")
 }
 
 // relational expressions or operators
-
+console.log("testing");
 // test the kids height to see if he can ride the ride
 
-var kidHeight = 42;
+var kidHeight = prompt("how tall are you in inches?");
 var sneakerLift=2;
+var minHeight=48
 
 // if the child is 48 inches or taller they can ride the ride
 
-if(kidHeight>=48){
- //code inside of here will run if child is 48 inches or taller
-  console.log("congrats you are tall enough to ride the coaster!!");
+if(kidHeight >=48){
+    //code inside of here will run if child is 48 inches or taller
+    console.log("congrats you are tall enough to ride the coaster!!");
+} else if (Number(kidHeight)+sneakerLift >=minHeight){
+    // this code will run if the child is tall enough while wearing sneakers
+    console.log("you are tall enough to ride the coaster if you wear your sneaker!\n cheater!")
 } else{
-console.log ("sorry you are too short");
+    console.log ("sorry you are too short");
 }
-
-
-console.log("this text is after the code block")
