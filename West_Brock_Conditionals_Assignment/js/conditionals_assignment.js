f530c31f5d76ccff597d3d05c7a339398444dde3
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-16 12:03:28 -0400

    monthly budget
    validate monthly budget

--- West_Brock_Conditionals_Assignment/js/conditionals_assignment.js
+++ West_Brock_Conditionals_Assignment/js/conditionals_assignment.js
@@ -9,15 +9,29 @@ Conditionals Assignment
 //alert("testing to see if this work!");
 
 // calculate if you can afford a dog
- 
+
+ var allowedPuppy= prompt ("Can you have a puppy where you live?\n Please type yes or no.");
+
+ // validate user input
+ if(allowedPuppy.toLowerCase()!="yes" && allowedPuppy.toLowerCase()!="no") {
+  //reprompt user
+  allowedPuppy = prompt("Please enter yes or no. Can you have a puppy where you live?");
+ }
+
 
  var puppyCost = prompt ("How much does the puppy cost?");
 
  //validate the user input
  if (isNaN(puppyCost)|| puppyCost===""){
+  //reprompt the user
    puppyCost= prompt ("Please don't leave this blank and enter a number.\n How much does the puppy cost?")
- }else console.log ("your puppy cost"+puppyCost)
+ }
 
+ var monthlyBudget = prompt ("How much your monthly budget allow you to spend?");
+ if (isNaN(monthlyBudget)|| monthlyBudget===""){
+  //reprompt the user
+  monthlyBudget=prompt ("Please only enter a number and don't leave this blank.\n How much does your monthly budget allow you to spend?")
+ }
 
 
 
