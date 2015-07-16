69f2e7e2841c493f2dbd1ea4212cbba29ea53af2
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-16 11:49:32 -0400

    puppy cost
    else if statements for puppy cost

--- West_Brock_Conditionals_Assignment/js/conditionals_assignment.js
+++ West_Brock_Conditionals_Assignment/js/conditionals_assignment.js
@@ -7,4 +7,18 @@ Conditionals Assignment
  */
 
 //alert("testing to see if this work!");
+
+// calculate if you can afford a dog
  
+
+ var puppyCost = prompt ("How much does the puppy cost?");
+
+ //validate the user input
+ if (isNaN(puppyCost)|| puppyCost===""){
+   puppyCost= prompt ("Please don't leave this blank and enter a number.\n How much does the puppy cost?")
+ }else console.log ("your puppy cost"+puppyCost)
+
+
+
+
+
