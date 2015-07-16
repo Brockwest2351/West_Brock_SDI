e9a24175168f34a41d1511b2c6d0068e042dc812
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-16 10:58:52 -0400

    conditionals assignment
    start of conditionals assignment

--- Conditionals_worksheet/js/conditionalsworksheet.js
+++ Conditionals_worksheet/js/conditionalsworksheet.js
@@ -14,26 +14,25 @@ var miles=200;
  var mpg= prompt ("how many miles per gallon does your car get?");
  console.log ("you get mpg "+mpg);
 
- var percentGas = prompt ("what percent is your gas gauge reading?");
+ var percentGas = percentGas/100
+ var prompt= ("what percent is your gas gauge reading?");
  console.log ("you have percent gas left" +percentGas/100);
 
  var tankGallons = prompt ("how many gallons does your tank hold?");
- console.log ("your tank holds " +tankGallons);
+ console.log ("your tank holds " +tankGallons+ "gallons");
 
  var milesPerTank;
  milesPerTank = mpg * tankGallons;
- console.log ("you get "+milesPerTank+ "miles per tank");
+ console.log ("you get "+milesPerTank+ " miles per tank");
 
- var milesLeft =(milesPerTank*tankGallons)/percentGas;
- console.log("you have "+milesLeft+" miles left in your tank");
+ var milesLeft =(milesPerTank*percentGas);
+ console.log("you have "+milesLeft+" gas left in your tank");
 
  var gallonsLeft= (milesLeft/tankGallons);
- console.log("you have "+gallonsLeft+ "gallons left in your tank");
+ console.log("you have "+gallonsLeft+ " gallons left in your tank");
 
-if (milesLeft<=200){
-console.log()
+if (milesLeft>=200){
+ console.log("You can make it!");}
+ else {console.log("You only have "+gallonsLeft+" You better stop for gas while you can")
 }
-
-
-
 
---
+++ West_Brock_Conditionals_Assignment/js/conditionals_assignment.js
@@ -0,0 +1,10 @@
+
+ /*
+Brock West
+Section 00
+16JUL15
+Conditionals Assignment
+ */
+
+//alert("testing to see if this work!");
+ 
