4fbe95183f4b3558e3f9d9edb94ac813fcc17b63
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-16 06:20:04 -0400

    starting files for day 5
    day 5 starting files

--- Conditionals_worksheet/js/conditionalsworksheet.js
+++ Conditionals_worksheet/js/conditionalsworksheet.js
@@ -9,28 +9,34 @@ conditionals
 //alert("testing to see if this work!");
 
 // last stop for gas
-var miles=200
+var miles=200;
  var mpg;
  var mpg= prompt ("how many miles per gallon does your car get?");
- console.log (mpg);
+ console.log ("you get mpg "+mpg);
 
  var percentGas;
  var percentGas = prompt ("what percent is your gas gauge reading?");
- console.log (percentGas);
+ console.log ("you have percent gas left" +percentGas/100);
 
  var tankGallons;
  var tankGallons = prompt ("how many gallons does your tank hold?");
- console.log (tankGallons);
+ console.log ("your tank holds " +tankGallons);
 
- var milesPerTank= mpg*=tankGallons;
- console.log (milesPerTank);
+ var milesPerTank= (mpg*=tankGallons);
+ console.log ("you get "+milesPerTank+ "miles per tank");
 
- if (milesPerTank>=200 && percentGas>= 50){
+ var milesLeft =(milesPerTank*=tankGallons)/percentGas;
+ console.log("you have "+milesLeft+" miles left in your tank");
+
+ var gallonsLeft= (milesLeft/tankGallons);
+ console.log("you have "+gallonsLeft+ "gallons left in your tank");
+
+ var currentGas=
+ console.log ("you can go "+currentGas);
+
+ if (milesPerTank>=200){
   console.log("You can make it without stopping for gas!")
- } else if (milesPerTank>200 && percentGas<=100){
-  console.log ("you only have "+mpg+" of gas in your car you better stop for gas");
- }
-
+ }else (milesPerTank<=200)
 // username and password
 
  var username= prompt("what is your username?");
@@ -40,8 +46,18 @@ var miles=200
 
  if (correctUsername + correctPassword){
   console.log("Welcome "+username);
- } else if(correctUsername)(username)
+ } else (correctUsername)(username);
  console.log("Username not found try again!");
 
+// ticket price
 
+var time=prompt ("what time are you attending the movie?");
+var age = prompt ("what is your age");
 
+var regularTicket= 12;
+var ageDiscount=7;
+var timeDiscount=-3;
+
+ if (regularTicket) {
+  console.log("the price of your ticket is 12");
+ } else if(age>10<55);
---
+++ day5/js/day5_steaks.js
@@ -0,0 +1,10 @@
+
+ /*
+Brock West
+Section 00
+16JUL15
+conditionals for steaks
+ */
+
+//alert("testing to see if this work!");
+
