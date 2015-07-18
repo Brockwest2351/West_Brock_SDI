356cafd86716225c9f62405d747f54ee1b4ca59a
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-18 08:37:25 -0400

    end for loops
    end for loops

--- day6/js/day6_forloops.js
+++ day6/js/day6_forloops.js
@@ -8,3 +8,22 @@ for loops
 
 //alert("testing to see if this work!");
 
+// basic for loop
+ // for (intialize counter; condition; increment of change)
+
+for (var i=10;i>0;i--){
+ console.log("the value of I is "+i);
+}
+
+
+ // breaks - if this command is run, your loop will stop
+
+for(var j=-10; j<5;j--){
+ console.log("the value of J is "+j);
+
+ // if the value of j is 3 stop my loop
+ if(j<-10000){
+  break;
+ }
+}
+ 
