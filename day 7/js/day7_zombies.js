0d4dadc7e7e299f01c409982dd62e0d2a60555e5
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-21 18:49:11 -0400

    for loop for 8 days
    for loop

--- day 7/js/day7_zombies.js
+++ day 7/js/day7_zombies.js
@@ -7,3 +7,30 @@ zombies
  */
 
 //alert("testing to see if this work!");
+
+ // we have 1 zombie here at full sail
+ // it can bite four people a day and turn them into zombies by the next day
+ // the CDC wants to know how many zombies there will be in 8 days
+
+ // create the givens
+
+ // how many zombies we have
+ var numZombies=1;
+
+ // number of bites a zombie can make each day
+ var numBites=4;
+
+ // what is our timeline
+ var days=8;
+
+ /*for(var i=1;i<=days;i++){
+  // how many people get infect everyday
+  var infectedPeople = numZombies*numBites;
+
+  // how many zombies will we have at the end of a day?
+  numZombies+=infectedPeople;
+
+  console.log("on day #"+i+" there will be "+numZombies+" zombies!");
+
+ }
+ */
