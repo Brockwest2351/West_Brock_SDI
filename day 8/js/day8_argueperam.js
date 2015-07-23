71cba59bd6c602241ddd89aad1472502a8c68459
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-23 19:01:07 -0400

    old school way
    super specific function only will calculate the area of a rectangle
    10x20

--- day 8/js/day8_argueperam.js
+++ day 8/js/day8_argueperam.js
@@ -7,3 +7,24 @@ arguements and perameters
  */
 
 //alert("testing to see if this work!");
+
+ // arguements-goes in the function call -"specific values"
+ // parameters- goes in the function defition "catch the agruements"
+
+ //create variables for width and height
+ var width=10;
+ var height=20;
+
+ // create a function to calculate the area of a rectangle
+
+
+function calcArea (){
+    // create variables
+    var width=10;
+    var height=20;
+    var area=width*height;
+    console.log("The area of the rectangle with the width of "+width+" and a height of "+height+" is "+area);
+
+}
+ calcArea();
+ calcArea();
