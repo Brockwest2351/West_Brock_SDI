a601140b2149db45adb0dc6e2c4861d9ac00db01
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-25 19:00:57 -0400

    procedures

--- day 9/js/day9_procedures.js
+++ day 9/js/day9_procedures.js
@@ -7,3 +7,28 @@ procedures
  */
 
 //alert("testing to see if this work!");
+
+// This is a normal function
+
+function calcArea(w,h){
+ var area=w*h;
+ return area;
+}
+ //function call to calcArea
+ // catch the return value
+
+ var results1= calcArea(10,20);
+
+ console.log("the area is "+results1)
+
+ // this is a procedure
+ // a list of steps to do
+ // returns nothing
+
+function calcPeri(w,h){
+ var peri=2*w+2*h;
+ console.log(peri)
+}
+ //function call for our procedure
+ // does not catch anything!
+ calcPeri(10,20);
