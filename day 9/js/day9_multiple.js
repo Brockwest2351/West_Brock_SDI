d3be4405e6e72624fb78e24fc11a722eb8996cc9
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-25 19:45:54 -0400

    multiple functions are working

--- day 9/js/day9_multiple.js
+++ day 9/js/day9_multiple.js
@@ -8,4 +8,52 @@ multiple
 
 //alert("testing to see if this work!");
 
+ // create variables for height and width
+ var height=8;
+ var width=6;
+
+ //create another variable for a base of a triangle
+ var base=5;
+
+
+ var resultsArea=calcArea(height,width);
+ console.log(resultsArea);
+ // create a function to calculate the perimeter of a rectangle
+
+ var calcPeri=function(h,w){
+  var periTotal=2*h+2*w;
+  return periTotal;
+ };
+
+ var resultsPeri=calcPeri(height,width);
+ console.log(resultsPeri);
+
+ // create a function to calculate the area of a rectangle
+
+ function calcArea (h,w){
+  var area=h*w;
+  return area;
+
+ }
+//create a function to calculate the area of a triangle
+ var triArea= function (b,h){
+  var area=.5* b*h;
+  return area;
+ };
+
+ //function call triangle area
+ var triAreaTotal=triArea(base,height);
+console.log(triAreaTotal);
+
+ // create a procedure that will console.log the rectangle perimeter and area
+
+ function printOut (p,a){
+  console.log("The perimeter is "+p);
+  console.log("the area of the rectangle is "+a)
+ }
+
+ //function call for printouts
+
+printOut(resultsPeri,resultsArea);
+
  
