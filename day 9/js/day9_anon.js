fe82e2c12adb8b19ff92e5d8f40d6fc62e3496b3
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-25 19:24:15 -0400

    anon function

--- day 9/js/day9_anon.js
+++ day 9/js/day9_anon.js
@@ -7,3 +7,23 @@ anonymous functions
  */
 
 //alert("testing to see if this work!");
+
+// basic anonymous function
+// var functionName = function(){};
+
+// extremely popular in JS and Jquery
+// important in object oriented programming
+// for now it is a matter of style choice between anon and regular functions.
+
+// create an anon function to calculate the area of a triangle
+
+var triArea = function(b,h){
+ var area=.5*b*h;
+ return area;
+
+};
+
+ // function call for anon function
+ // function call must be after the definition
+ var results=triArea(10,20);
+ console.log(results);
