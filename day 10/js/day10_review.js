bacdd01da8bd539a4313e809f49e221ee9c267f3
Brockwest2351 <Brock@Brocks-MacBook-Pro.local>
2015-07-28 18:11:43 -0400

    finished pizza summary used master function call

--- day 10/js/day10_review.js
+++ day 10/js/day10_review.js
@@ -18,20 +18,25 @@ var pizzaRadius= prompt("Lets figure how much the pizza cost.\nWhat is the radiu
 while(isNaN(pizzaRadius)||pizzaRadius===""){
     pizzaRadius=prompt("Please enter a number and do not leave blank.\nWhat is the radius of the pizza?");
 }
+pizzaRadius=Number(pizzaRadius);
 
 var pizzaCost= prompt("How much does your pizza cost in total?");
  while(isNaN(pizzaCost)||pizzaCost===""){
      pizzaCost=prompt("Please enter a number and do not leave blank.\nPlease enter pizza cost?");
  }
+pizzaCost=Number(pizzaCost);
 
 var pizzaSlices= prompt("How many slices of pizza are there?");
  while(isNaN(pizzaSlices)||pizzaSlices===""){
      pizzaSlices=prompt("please enter a number and do not leave this blank\nPlease enter total number of slices?");
  }
+pizzaSlices=Number(pizzaSlices);
+
+
 
  // create a function to calculate the area of pizza
 
-function pizzaArea(r){
+function pizzaArea1(r){
     // radius of circle r*r*PI
     var area= Math.pow(r,2)*Math.PI;
     return area;
@@ -55,5 +60,25 @@ function costPerSlice (price,slice){
     return cost;
 
 }
+// create a function that launches all of the other functions
+ function masterPizza(rad,cost,slices){
+     var pizzaArea= pizzaArea1(rad);
 
- 
+     var sqInchCost= pizzaSqInchCost(cost,pizzaArea);
+
+     var slicePrice=costPerSlice(slices,cost);
+
+     return[sqInchCost,slicePrice]
+ }
+
+
+// function call to the master function
+ var master= masterPizza(pizzaRadius,pizzaCost,pizzaSlices);
+
+
+// print out results
+
+ console.log("Your cost $"+master[0]+"per sq inch of pizza or $"+master[1]+"per slice");
+
+
+
